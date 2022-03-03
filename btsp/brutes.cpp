235d3d3bdbfca0898564506eab47c7e855191b23
J-Salt <js730674@sju.edu>
2022-03-03 12:28:02 -0500

    updated 5 loop

--- btsp/brutes.cpp
+++ btsp/brutes.cpp
@@ -59,23 +59,32 @@ bool isValidPermutation(int i, int j, int k, int x, int y) {
 long bruteForce5Loops(double A[][2], int n) {
   assert(n == 5);  //only works for n=5
   long count = 0, countValid = 0;
-  double tCost = 0, bestCost = INT_MAX;
+  double currCost = 0, bestCost = INT_MAX;
   double(*tmp)[2] = new double[n][2];
+  double(*cpy)[2] = new double[n][2];
   for (int i = 0; i < n; i++) {//first element
     for (int j = 0; j < n; j++) {//second element
       for (int k = 0; k < n; k++) {//third element
         for (int x = 0; x < n; x++) {//fourth element
           for (int y = 0; y < n; y++) {//fifth element
             if (isValidPermutation(i,j,k,x,y)) {
-              tCost += costSingle(A, i, j);
-              tCost += costSingle(A, j, k);
-              tCost += costSingle(A, k, x);
-              tCost += costSingle(A, x, y);
-              tCost += costSingle(A, y, i);
+              cpy[0][0] = A[i][0];
+              cpy[0][1] = A[i][1];
+              cpy[1][0] = A[j][0];
+              cpy[1][1] = A[j][1];
+              cpy[2][0] = A[k][0];
+              cpy[2][1] = A[k][1];
+              cpy[3][0] = A[x][0];
+              cpy[3][1] = A[x][1];
+              cpy[4][0] = A[y][0];
+              cpy[4][1] = A[y][1];
+
+              //cout << i << ", " << j << ", " << k << ", " << x << ", " << y << endl;
+              currCost = cost(cpy, n);
               countValid++;
-              if (tCost <= bestCost) {
-                bestCost = tCost;
-                copy(tmp,n,A);
+              if (currCost <= bestCost) {
+                bestCost = currCost;
+                copy(tmp,n,cpy);
               }
             }
             count++;
@@ -84,6 +93,8 @@ long bruteForce5Loops(double A[][2], int n) {
       }
     }
   }
+  cout << "BEST COST: " << bestCost << endl;
+  cout << count << " valid: " << countValid << endl;
   copy(A, n, tmp);
   return count;
 }
